68347a6df238bc7c55b43ab0f1b6a3d14a4bcdef
Noor Muhammad <connectwithnoor1@gmail.com>
2019-12-12 00:30:14 +0500

    set list of items for static precaching

--- sw.js
+++ sw.js
@@ -1,13 +1,31 @@
+const staticCache_v1 = [
+  '/',
+  '/index.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  '/src/images/logo.png',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://code.getmdl.io/1.3.0/material.blue_grey-pink.min.css'
+];
+
 self.addEventListener('install', event => {
   console.log('service worker installed', event);
 
   // waitUntill takes a promise
   event.waitUntil(
     caches
-      .open('static')
-      .then(staticCache => {
+      .open('static-v1')
+      .then(cache => {
         console.log('service worker, precaching app shell');
-        staticCache.add('/src/js/app.js');
+        cache
+          .addAll(staticCache_v1)
+          .then(_ => console.log('service worker, precached app shell'))
+          .catch(err => console.log('service worker, precaching failed', err));
       })
       .catch(err => {
         console.log('service worker, precaching failed', err);
@@ -21,5 +39,15 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => {
+        // even thought the response is not found on cache it still executes
+        // the then block instead of catch block by response as a undefined bcz, technically
+        // undefined is not ana error.
+        return response ? response : fetch(event.request);
+      })
+      .catch(err => console.log('fetching cache error', err))
+  );
 });
